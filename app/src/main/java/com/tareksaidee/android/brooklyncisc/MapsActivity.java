c8b1977d8682dcef127a90fb81e193f578be7353
Tarek <tareksaidee@hotmail.com>
2016-09-11 23:03:50 -0400

    Update map to point at Brooklyn College

--- app/src/main/java/com/tareksaidee/android/brooklyncisc/MapsActivity.java
+++ app/src/main/java/com/tareksaidee/android/brooklyncisc/MapsActivity.java
@@ -29,7 +29,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker at Brooklyn College
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -38,9 +38,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Brooklyn College and move the camera
+        LatLng BC = new LatLng(40.630965, -73.954203);
+        mMap.addMarker(new MarkerOptions().position(BC).title("Brooklyn College"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(BC));
     }
 }
