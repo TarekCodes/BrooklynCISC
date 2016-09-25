5c5f531e737af2f64a8613b20536eef9d076c0d3
Tarek <tareksaidee@hotmail.com>
2016-09-24 23:44:52 -0400

    Change routes

--- app/src/main/java/com/tareksaidee/android/brooklyncisc/Routes.java
+++ app/src/main/java/com/tareksaidee/android/brooklyncisc/Routes.java
@@ -1,9 +1,12 @@
 package com.tareksaidee.android.brooklyncisc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.tareksaidee.android.brooklyncisc.fragments.CmElec;
 import com.tareksaidee.android.brooklyncisc.fragments.CmOther;
@@ -69,5 +72,11 @@ public class Routes extends AppCompatActivity {
             default:
                 return R.layout.csmajor;
         }
+    }
+
+    public void goToCourse(View view) {
+        Intent intent = new Intent(this, Courses.class);
+        intent.putExtra("code", ((Button) view).getText().toString());
+        startActivityForResult(intent, 0);
     }
 }
--- app/src/main/java/com/tareksaidee/android/brooklyncisc/fragments/PdcMnReq.java
+++ app/src/main/java/com/tareksaidee/android/brooklyncisc/fragments/PdcMnReq.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 import com.tareksaidee.android.brooklyncisc.R;
 
 public class PdcMnReq extends Fragment {
-    public PdcMnReq(){}
+    public PdcMnReq() {
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
